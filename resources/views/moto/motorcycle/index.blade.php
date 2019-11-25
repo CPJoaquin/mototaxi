985babdc5414837460449c105ee14510de1bd125
joaquin <cpjoaquin.26795@gmail.com>
2019-11-25 19:42:26 -0400

    views, motorcycle

--- resources/views/moto/motorcycle/index.blade.php
+++ resources/views/moto/motorcycle/index.blade.php
@@ -1,88 +1,79 @@
 @extends('layouts.app')
 @section('content')
-    @include('layouts.menu')
-    <div class="card text-center container">
-        <h2>Registro de Motocicletas</h2>
-        <table id="example" class="table table-striped table-bordered" style="width:100%">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Position</th>
-                        <th>Office</th>
-                        <th>Age</th>
-                        <th>Start date</th>
-                        <th>Salary</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td>61</td>
-                        <td>2011/04/25</td>
-                        <td>$320,800</td>
-                    </tr>
-                    <tr>
-                        <td>Garrett Winters</td>
-                        <td>Accountant</td>
-                        <td>Tokyo</td>
-                        <td>63</td>
-                        <td>2011/07/25</td>
-                        <td>$170,750</td>
-                    </tr>
-                    <tr>
-                        <td>Ashton Cox</td>
-                        <td>Junior Technical Author</td>
-                        <td>San Francisco</td>
-                        <td>66</td>
-                        <td>2009/01/12</td>
-                        <td>$86,000</td>
-                    </tr>
-                    <tr>
-                        <td>Cedric Kelly</td>
-                        <td>Senior Javascript Developer</td>
-                        <td>Edinburgh</td>
-                        <td>22</td>
-                        <td>2012/03/29</td>
-                        <td>$433,060</td>
-                    </tr>
-                    <tr>
-                        <td>Airi Satou</td>
-                        <td>Accountant</td>
-                        <td>Tokyo</td>
-                        <td>33</td>
-                        <td>2008/11/28</td>
-                        <td>$162,700</td>
-                    </tr>
-                    <tr>
-                        <td>Brielle Williamson</td>
-                        <td>Integration Specialist</td>
-                        <td>New York</td>
-                        <td>61</td>
-                        <td>2012/12/02</td>
-                        <td>$372,000</td>
-                    </tr>
-                    <tr>
-                        <td>Herrod Chandler</td>
-                        <td>Sales Assistant</td>
-                        <td>San Francisco</td>
-                        <td>59</td>
-                        <td>2012/08/06</td>
-                        <td>$137,500</td>
-                    </tr>
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <th>Name</th>
-                        <th>Position</th>
-                        <th>Office</th>
-                        <th>Age</th>
-                        <th>Start date</th>
-                        <th>Salary</th>
-                    </tr>
-                </tfoot>
-            </table>
+@include('layouts.menu')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md">
+            <div class="card">
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+                    <section class="content-header">
+                        <h1 class="pull-left">Registro de vehiculos</h1>
+                        <div class="d-flex">
+                            <a class="btn btn-primary ml-auto" style="margin-right: 5px;"
+                                href="{{ route('moto.create')}}"><i class="fas fa-plus-circle"></i> Nuevo</a>
+                        </div>
+                        <br>
+                    </section>
+                    {!! Form::open(['route' => 'moto.index', 'method' => 'GET', 'class' => ''])!!}
+                    <div id="input-group">
+                        {!!Form::text('buscar', null, ['class' => 'form-control', 'placeholder' => 'Buscar','style' =>
+                        'text-transform:uppercase;',
+                        'onkeyup' => 'javascript:this.value=this.value.toUpperCase();'])!!}
+                    </div>
+                    {!!Form::close()!!}
+                    <br>
+                    <div class="table-responsive">
+                        <table id="grilla" class="table table-bordered table-striped table-hover">
+                            <thead>
+                                <tr class="text-center">
+                                    <th>Placa</th>
+                                    <th>Color</th>
+                                    <th>Conductor</th>
+                                    <th>Marca</th>
+                                    <th>Modelo</th>
+                                    <th width="50px">Opciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($motos as $item)
+                                
+                                        <tr>
+                                            <td>{!! $item->placa!!}</td>
+                                            <td>{!! $item->color !!}</td>
+                                            <td>{!! $conductors["$item->user_id"] !!}</td>
+                                            <td>{!! $item->marc!!}</td>
+                                            <td>{!! $item->model !!}</td>
+                                            <td class="text-center">
+                                                <div id="btn-group">
+                                                    {!! Form::open(['route' => ['moto.destroy', $item->id], 'method' => 'DELETE', 'id' => 'formDelete']) !!}
+                                                    <button class="btn-sm btn-primary" data-toggle="dropdown" type="button">
+                                                        <span data-feather="settings"> <i class="fas fa-align-left"></i> 
+                                                        </button>
+                                                    <div class="dropdown-menu">
+                                                        <a href="{!! route('moto.edit', [$item->id])!!}"class="dropdown-item btn btn-warning btn-sm">
+                                                            <span data-feather="list"></span> Editar
+                                                        </a>
+                                                        <button type="submit" class="dropdown-item btn btn-danger btn-sm"
+                                                            onclick="return confirm('Está seguro de eliminar este registro')"><span
+                                                                data-feather="trash-2"></span> Eliminar</button>
+                                                    </div>
+                                                    {!! Form::close() !!}
+                                                </div>
+                                            </td>
+                                        </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        {!! $motos->links() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-   
+</div>
 @endsection
